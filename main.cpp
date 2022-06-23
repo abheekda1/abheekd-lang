c5c99fa4a4ddb2ecc598188415241ac91a7ad278
Abheek Dhawan <abheekd@protonmail.com>
2022-06-23 14:12:24 -0500

    feat: move binop to token, use strings for binops, attempt to improve types for binops

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@
 #include "Parser/Parser.hpp"
 #include "Token/Token.hpp"
 
+const char *out_file = "out.ll";
+
+// todo: add much better logging for parsed stuff
 static void HandleDefinition() {
     if (auto FnAST = Parser::ParseFuncDefinition()) {
         if (auto *FnIR = FnAST->codegen()) {
-            fprintf(stderr, "Read function definition:\n");
-            FnIR->print(llvm::errs());
-            fprintf(stderr, "\n");
+            // fprintf(stderr, "Read function definition:\n");
+            // FnIR->print(llvm::errs());
+            // fprintf(stderr, "\n");
         }
-        printf("Parsed a function definition.\n");
+        //printf("Parsed a function definition.\n");
     } else {
         // Skip token for error recovery.
         Parser::getNextToken();
@@ -21,12 +24,12 @@ static void HandleDefinition() {
 static void HandleExtern() {
     if (auto ProtoAST = Parser::ParseExtern()) {
         if (auto *ProtoIR = ProtoAST->codegen()) {
-            fprintf(stderr, "Read proto definition:\n");
-            ProtoIR->print(llvm::errs());
-            fprintf(stderr, "\n");
+            // fprintf(stderr, "Read proto definition:\n");
+            // ProtoIR->print(llvm::errs());
+            // fprintf(stderr, "\n");
         }
 
-        printf("Parsed an extern\n");
+        //printf("Parsed an extern\n");
     } else {
         // Skip token for error recovery.
         Parser::getNextToken();
@@ -37,11 +40,11 @@ static void HandleTopLevelExpression() {
     // Evaluate a top-level expression into an anonymous function.
     if (auto StAST = Parser::ParseStatement()) {
         if (auto *FnIR = StAST->codegen()) {
-            fprintf(stderr, "Read top-level expr (statement):\n");
-            FnIR->print(llvm::errs());
-            fprintf(stderr, "\n");
+            // fprintf(stderr, "Read top-level expr (statement):\n");
+            // FnIR->print(llvm::errs());
+            // fprintf(stderr, "\n");
         }
-        printf("Parsed a top-level expr (statement)\n");
+        // printf("Parsed a top-level expr (statement)\n");
     } else {
         // Skip token for error recovery.
         Parser::getNextToken();
@@ -89,14 +92,16 @@ int main() {
     Lexer("extern puts(in : s1*) : s4;\n"
           "func main() : s4 {\n"
           "\tputs(\"hello world!\");\n"
-          "\treturn \"heheheha\";\n"
-          "}\n"
-          "func thing(arg1 : s4, arg2 : s4) : s4 {\n"
-          "\targ1 + arg2;\n"
-          "\treturn arg1 + arg2 * 6.0;\n"
-          "};\n"
-          "\n"
-          "thing(1.0, 2.0);");
+          "\treturn 0;\n"
+          "}\n");
+          // "func thing(arg1 : s4, arg2 : s4) : s4 {\n"
+          // "\targ1 + arg2;\n"
+          // "\treturn arg1 + arg2 * 6.0;\n"
+          // "};\n"
+          // "\n"
+          // "thing(1.0, 2.0);"); // todo: fix function stuff bc this call
+                               // is nested within the function instead
+                               // of being outside
 
     std::cout << "SOURCE:\n---\n" << Lexer::Source << "\n---\n" << std::endl;
 
@@ -132,5 +137,6 @@ int main() {
 
     MainLoop();
 
-    SaveModuleToFile("place.ll");
+    SaveModuleToFile(out_file);
+    std::cout << "saved compiled LLVM IR to \"" << out_file << "\"!\n" << std::flush;
 }
